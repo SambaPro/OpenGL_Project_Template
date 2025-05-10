9a92006f7f7ae68df2cb09f47949740b3089bb50
SambaPro <sambasteam@mail.com>
2025-05-10 20:02:20 +0100

    Added GLFW and Glad dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,33 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 
 int main()
 {
+    // GLFW Initialisation
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Window", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    // Glad Initialisation
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }    
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
